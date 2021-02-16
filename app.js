65310d5e90537318f0c55901237969bd0f8ecec8
Nasr-Eddine Behilil <nasr-eddine15@hotmail.com>
2021-02-16 00:12:58 -0500

    Add a Procfile and prepare app for deployment

--- app.js
+++ app.js
@@ -269,6 +269,8 @@ app.get("/posts/:postName/:postId", function(req, res) {
   });
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
